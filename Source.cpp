138cba6938ce15d4c66b07af03a15208c9434a38
tsaiminghsu <43085867+tsaiminghsu@users.noreply.github.com>
2019-06-06 14:30:49 +0800

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace std;
+using namespace cv;
+
+int main() {
+	Mat src = imread("lena_gray.gif");
+	Mat img1;
+	int i, j;
+	if (!src.data) {
+		cout << "無法開啟" << end1;
+		return -1;
+	}
+	for (i = 0; i < src.row; i++) {
+		for (j = 0; j < src.col; j++) {
+			img1.at<uchar>(i, j) = 255 - src.at<uchar>(i, j);
+		}
+	}
+	imshow("輸出", img1);
+}
